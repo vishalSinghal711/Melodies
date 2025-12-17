a893b57369450dd2e744213a451e5d9e3a7ee9bc
Vishal Singhal <79749739+vishalSinghal711@users.noreply.github.com>
2025-12-17 20:31:48 +0530

    Update app.js

--- music_apis/app.js
+++ music_apis/app.js
@@ -31,20 +31,7 @@ app.use(cors());
 // to enable Json conversion for every request,response
 app.use(express.json());
 
-// Adding Middlewares for specific matching request
-app.get("/", (req, res) => {
-  res.send(`
-    <html>
-      <head>
-        <title>Server Status</title>
-      </head>
-      <body>
-        <h2>✅ Server is running</h2>
-        <p>Port: ${process.env.PORT || 2345}</p>
-      </body>
-    </html>
-  `);
-});
+
 
 app.use("/", require("./routes/user.routes")); // / root or Home
 app.use("/", require("./routes/music.routes"));
